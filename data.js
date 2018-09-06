04a03dc61bf6771427f792e7e09a79d19dd8991c
klequis <klequis@gmail.com>
2018-09-05 21:19:24 -0700

    initial

---
+++ data.js
@@ -0,0 +1,26 @@
+export const conditions = {
+  icon_url: 'https://s3-us-west-2.amazonaws.com/weather-media/cloudy.gif',
+  weather: 'Mostly Cloudy',
+  temp: 80,
+  dewpoint: 43,
+  feelslike: 80,
+  pressure: '1010 mp',
+  pressure_trend: '+',
+  relative_humidity: '56%',
+  uv: 0,
+  wind_dir: 'SSE',
+  windspeed: '4 mph',
+  windgust: '10 mph',
+}
+
+export const forecast = [
+  { date: '9/4', temp: 80 },
+  { date: '9/5', temp: 90 },
+  { date: '9/6', temp: 70 },
+]
+
+export const location = {
+  elevation: 103.9,
+  latitude: 37.66,
+  longitude: -121.88
+}
